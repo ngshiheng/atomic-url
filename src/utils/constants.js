2934414d0205e1f609b01782d81c32a4a6358668
Jerry Ng <ngshiheng@gmail.com>
2021-12-29 00:32:57 +0800

    style: update html content

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -8,23 +8,24 @@ export const LANDING_PAGE_HTML = `
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>CF URL Shortener</title>
+        <title>Atomic URL</title>
+        <link rel="icon" type="image/png" href="https://img.icons8.com/external-dreamstale-green-shadow-dreamstale/50/000000/external-atom-energy-and-ecology-dreamstale-green-shadow-dreamstale.png">
         <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;500;700&display=swap" rel="stylesheet">
         <script src="https://kit.fontawesome.com/15181efa86.js" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="https://unpkg.com/bulma@0.9.0/css/bulma.min.css" />
-        <link rel="stylesheet" type="text/css" href="../css/register.css">
         <script>
             const submitURL = () => {
-                document.getElementById("status").innerHTML="Creating short URL..."
+                document.getElementById('status').innerHTML = 'Creating short URL...'
             
-                const originalUrl = document.getElementById("url").value
+                const originalUrl = document.getElementById('url').value
                 const body = JSON.stringify({ originalUrl })
-                
-                fetch('/api/url', {method: "POST", body })
-                    .then(data => data.json())
-                    .then(data => {
-                        document.getElementById("status").innerHTML="Your short URL: " + data.shortUrl
-                    } )
+            
+                fetch('/api/url', { method: 'POST', body })
+                    .then((data) => data.json())
+                    .then((data) => {
+                        document.getElementById('status').innerHTML =
+                            'Your short URL: ' + data.shortUrl
+                    })
             }
         </script>
     </head>
@@ -34,10 +35,12 @@ export const LANDING_PAGE_HTML = `
                 <div class="column is-8 is-offset-2 register">
                     <div class="columns">
                         <div class="column left">
-                            <h1 class="title is-1">CF URL Shortener</h1>
+                            <h1 class="title is-1">Atomic URL</h1>
                             <h2 class="subtitle colored is-4">A URL shortener POC built using Cloudflare Worker.</h2>
-                            <p>Designing a URL shortener such as TinyURL and Bitly is one of the most common System Design interview questions in software engineering.</p>
-                            <p>While meddling around with Cloudflare Worker, it gave me an idea to build an actual URL shortener that can be used by anyone.</p>
+                            <p>Designing a URL shortener such as <a href="https://tinyurl.com/">TinyURL</a> and <a href="https://bitly.com/">Bitly</a> is one of the most common System Design interview questions in software engineering.</p>
+                            </br>
+                            <p>While meddling around with <a href="https://workers.cloudflare.com/">Cloudflare Worker</a>, it gave me an idea to build an actual URL shortener that can be used by anyone.</p>
+                            </br>
                             <p>This is a proof of concept (POC) of how one builds an actual URL shortener service using serverless computing.</p>
                         </div>
                         <div class="column right has-text-centered">
@@ -59,7 +62,7 @@ export const LANDING_PAGE_HTML = `
                     <nav class="level">
                         <div class="level-right">
                             <small class="level-item" style="color: var(--textLight)">
-                            &copy; CF URL Shortener. All Rights Reserved.
+                            &copy; Atomic URL by jerrynsh.com. All Rights Reserved.
                             </small>
                         </div>
                     </nav>
