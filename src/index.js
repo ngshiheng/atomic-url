fd04eb00b0f42ec28c7a964880713f42a1678a1c
Jerry Ng <ngshiheng@gmail.com>
2022-10-04 23:02:40 +0800

    refactor: move landing page html to index.html

--- src/index.js
+++ src/index.js
@@ -1,29 +1,21 @@
+import LANDING_PAGE_HTML from './index.html'
+
 import { Router } from 'itty-router'
 import { createShortUrl } from './handlers/createShortUrl'
 import { redirectShortUrl } from './handlers/redirectShortUrl'
 import { shortUrlCacheMiddleware } from './middleware/shortUrlCache'
 import { siteFilterMiddleware } from './middleware/siteFilter'
-import { LANDING_PAGE_HTML } from './utils/constants'
 
 const router = Router()
 
 // GET landing page html
-router.get('/', () => {
-    return new Response(LANDING_PAGE_HTML, {
-        headers: {
-            'content-type': 'text/html;charset=UTF-8',
-        },
-    })
-})
+router.get('/', () => new Response(LANDING_PAGE_HTML, { headers: { 'content-type': 'text/html;charset=UTF-8' } }))
 
 // GET redirects short URL to its original URL.
 router.get('/:text', redirectShortUrl)
 
 // POST creates a short URL that is associated with its an original URL.
 router.post('/api/url', shortUrlCacheMiddleware, siteFilterMiddleware, createShortUrl)
-
-// 404 for everything else.
-router.all('*', () => new Response('Not Found', { status: 404 }))
 
 // All incoming requests are passed to the router where your routes are called and the response is sent.
 addEventListener('fetch', (e) => {
